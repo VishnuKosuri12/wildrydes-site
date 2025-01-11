f0ea228d59f206507216e766551522d90643cc60
VK <167237408+VishnuKosuri12@users.noreply.github.com>
2025-01-11 05:38:54 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_JGO0VaRRl', // e.g. us-east-2_JGO0VaRRl
-        userPoolClientId: '1ee5jhik0d694g4hl57gda2ule', // e.g. kjnakorji6muikisn4lqh1ps
+        userPoolId: 'us-east-2_IckFObiVU', // e.g. us-east-2_JGO0VaRRl
+        userPoolClientId: '1mmbmrmebp1vt5i773ssgvrc5m', // e.g. kjnakorji6muikisn4lqh1ps
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
